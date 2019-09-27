c076d0df742c507eaca3a55ad53b6a5e582c9284
AadityaDev <adityaverma.dev@gmail.com>
2019-09-27 17:28:50 +0530

    package version updated

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.ae1e9c8383c7c72f6da7297b8ce14528.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.9e4b1ee5cd80b00a41ae6b982d6dd55a.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* eslint-disable */
 
