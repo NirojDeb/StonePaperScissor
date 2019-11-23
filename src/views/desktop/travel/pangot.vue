96125bb622c45cc4059028b2ecc40bbe9db01eb4
AadityaDev <adityaverma.dev@gmail.com>
2019-11-24 00:32:32 +0530

    changes added

---
+++ src/views/desktop/travel/pangot.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="auth-page with-pattern">
+    <h1>Desktop login</h1>
+    <div class="flex flex-wrap h-screen items-center">
+      des
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: 'TravelPangotPage',
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  computed: {
+    ...mapGetters(['request']),
+  },
+  methods: {
+    ...mapActions(['login']),
+  },
+};
+</script>
---
+++ src/views/mobile/travel/pangot.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="auth-page with-pattern">
+    <h1>Mobile login</h1>
+    <div class="flex flex-wrap h-screen items-center">
+      desi
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: 'TravelPangotPage',
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  computed: {
+    ...mapGetters(['request']),
+  },
+  methods: {
+    ...mapActions(['login']),
+  },
+};
+</script>
